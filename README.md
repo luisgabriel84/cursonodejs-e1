6ea4d2310115dda536de324195426fa9d25683f4
Gabriel Rodriguez <luisgabriel84@gmail.com>
2019-05-19 17:42:33 -0500

    Terminando la primera entrega acorde a los criterios de aceptacion

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cursonodejs-e1
+# Primera entrega curso de node.js
+
+*Ing. Luis Gabriel Rodriguez*
+
+
+
+### Ejecutar el listado de cursos 
+```javascript
+node index
+```
+
+### Inscripción exitosa
+
+```javascript
+node index inscribir -i=1001 -n="Nombre del estudiante" -c=80999222
+```
+
+### Inscripción Fallida
+```javascript
+node index inscribir -i=1007 -n="Nombre del estudiante" -c=80999222
+```
--- index.js
+++ index.js
@@ -1,49 +1,131 @@
+const opciones ={
+    idCurso:{
+        demand: true,
+        alias: 'i'
+    },
+    nombre:{
+        demand: true,
+        alias: 'n'
+    },
+    cedula:{
+        demand: true,
+        alias: 'c'
+    },
+}
+
+const argv = require('yargs')
+            .command('inscribir','Inscribir un estudiante en el curso',opciones)
+            .argv
+
 //controlador global de la aplicacion
 var cursosController = (function() {
 
-    var CursosOfertados =[
+    const CursosOfertados =[
         {   id:1001,
-            name: "Introduccion a La programación",
-            duracion: 40
+            name: "Introducción a La programación",
+            duracion: 40,
+            valor:350000
         },
         {   id:1002,
-            name: "Introduccion a la ingenieria de sistemas",
-            duracion: 30
+            name: "Introducción a la ingeniería de sistemas",
+            duracion: 30,
+            valor:380000
         },
         {   id:1003,
-            name: "Calculo integral",
-            duracion: 40
+            name: "Cálculo integral",
+            duracion: 40,
+            valor:400000
+        },
+        {   id:1004,
+            name: "Teología",
+            duracion: 20,
+            valor:100000
+        },
+        {   id:1005,
+            name: "Constitución Política",
+            duracion: 20,
+            valor:100000
         }
     ];
+
+    /* Buscar un curso +*/
+    let findCourse = (id) =>{
+        const course = CursosOfertados.find(curso => curso.id === id);
+        return course;
+    };
+
+    /* Generar archivo de inscripcion*/
+    let  writeFile = (course, name, cedula) =>{
+        const fs = require('fs');
+
+        let texto_inscripcion = 'EL ESTUDIANTE: '+ name +' \nCÉDULA: ' + cedula + '\n'+
+        "FUE INSCRITO EN EL CURSO: "+ course.name + "\nDURACIÓN: "+course.duracion + "\nVALOR: $" + course.valor;
+        
+        fs.writeFile('inscripcion-' + cedula + '.txt',texto_inscripcion,(err)=>{
+            if(err) throw(err);
+            console.log('se ha creado el archivo de inscripción');
+        });
+    }
+
     return {
         getCourses: function(){
             return CursosOfertados;
+        },
+        inscribirEstudiante(args){
+            let resultCourse = findCourse(args.i);
+            if(resultCourse){
+                console.log("\nESTÁS INTERESADO EN EL CURSO: "+ resultCourse.id +" "+  resultCourse.name + ": DURACIÓN"+ resultCourse.duracion + " - VALOR: "+resultCourse.duracion );
+                writeFile(resultCourse, args.n, args.c);
+                return true;
+            }else{
+                console.log("\nNo se encontro ningún curso con ese id: "+  args.i + "\nEste es el listado de cursos ");
+                return false;
+            }
         }
     }
 })();
 
-
-
-//Controlador global de la aplicacion
+//Controlador global de la aplicación
 var controller = (function(cursosCtrl){
-
     let  courses = cursosController.getCourses();
+
+    let validarEntrada = (attr)=>{
+        if(attr.i && attr.n && attr.c){
+            return true;
+        }
+        return false;
+    };
+
     return {
+        // Inicializar  la aplicación
+        init: function(args){
+            if(!validarEntrada(args)){
+                console.log("[ Bienvenido al listado de cursos a continuación se presentan los cursos ofertados ] \n----------------");
+                this.verCursos();
+            }else{
+                if(cursosController.inscribirEstudiante(args)){
+                    console.log("***************** \nEstudiante Inscrito");
+                }else{
+                    console.log("----------------");
+                    this.verCursos();
+                }
+            }
+        },
+
         //Muestra el listado de cursos
         verCursos: function() {
             for (i = 0; i < courses.length; ++i) {
                 (function (i) {
                     setTimeout(function () {
-                      console.log(courses[i].id + " ) " + courses[i].name+ " Duración: " + courses[i].duracion );
+                      console.log(courses[i].id + " ) " + courses[i].name+ " Duración: " + courses[i].duracion+ " - Valor: $" + courses[i].valor );
                     }, 2000*i);
                   })(i);
               }
-        }
+        },
     }
 
 
 })(cursosController);
 
-
-//historia 1
-controller.verCursos();
+//Ejecutar la aplicación
+controller.init(argv);
